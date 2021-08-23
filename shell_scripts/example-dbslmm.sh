f888f4d0fb78d6b461c132548c7bc2ba567e7b70
Frederick Boehm <frederick.boehm@gmail.com>
2021-08-23 14:32:35 -0400

    fixed (?) filename in two places

--- shell_scripts/example-dbslmm.sh
+++ shell_scripts/example-dbslmm.sh
@@ -22,8 +22,8 @@ est=../test_dat/out/summary_gemma_chr
 InterPred=../test_dat/out/internal_pred_chr
 ## plink 1.9
 plink=/usr/cluster/bin/plink-1.9
-${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.assoc.dbslmm.txt 1 2 4 sum --out ${InterPred}${chr}
+${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.dbslmm.txt 1 2 4 sum --out ${InterPred}${chr}
 ## plink 2
 plink=plink2
-${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.assoc.dbslmm.txt 1 2 4 cols=+scoresums --out ${InterPred}${chr}
+${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.dbslmm.txt 1 2 4 cols=+scoresums --out ${InterPred}${chr}
 
