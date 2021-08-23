cf8e97aa16074210fb8f019a900dc025b0dda3aa
Frederick Boehm <frederick.boehm@gmail.com>
2021-08-23 13:31:21 -0400

    fixed typos

--- shell_scripts/example-dbslmm.sh
+++ shell_scripts/example-dbslmm.sh
@@ -20,9 +20,9 @@ bfilete=../test_dat/test_chr
 est=../test_dat/out/summary_gemma_chr
 InterPred=../test_dat/out/internal_pred_chr
 ## plink 1.9
-plink=plink1.9
+plink=plink-1.9
 ${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.assoc.dbslmm.txt 1 2 4 sum --out ${InterPred}${chr}
 ## plink 2
 plink=plink2
-${plink} --bfile ${bfilete} --score ${est}${chr}.assoc.dbslmm.txt 1 2 4 cols=+scoresums --out ${InterPred}${chr}
+${plink} --bfile ${bfilete}${chr} --score ${est}${chr}.assoc.dbslmm.txt 1 2 4 cols=+scoresums --out ${InterPred}${chr}
 
