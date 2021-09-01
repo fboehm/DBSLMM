7250c3f11c03ca09b4c2e6b806162a731b50cc3a
Frederick Boehm <frederick.boehm@gmail.com>
2021-09-01 13:05:29 -0400

    added helper functions for calculating asymptotic variance matrix

--- scr/calc_asymptotic_variance.cpp
+++ scr/calc_asymptotic_variance.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <armadillo>
+#include "calc_asymptotic_variance.h"
 
 //' Calculate the asymptotic variance for the beta hat estimator
 //' 
@@ -17,9 +18,18 @@ arma::mat calc_asymptotic_variance(arma::mat Xl_training,
                                    arma::mat Xl_test, 
                                    arma::mat Xs_test,
                                    double sigma2_s,
-                                   arma::vec y){
+                                   arma::vec y_training){
   arma::mat Hinv = calc_Hinv(Xs_training, sigma2_s);
-  
+  arma::mat var_bl = calc_var_betal(Xl = Xl_training, 
+                                    Hinv = Hinv, 
+                                    y = y_training);
+  arma::mat var_bs = calc_var_betas(Xl = Xl_training, 
+                                    Xs = Xs_training, 
+                                    Hinv = Hinv, 
+                                    sigma2_s = sigma2_s,
+                                    y = y_training, 
+                                    var_bl = var_bl);
+  arma::mat result = Xl_test.t() * var_bl * Xl_test + Xs_test.t() * var_bs * Xs_test;
   return(result);
 }
 
@@ -55,3 +65,27 @@ arma::mat calc_var_betal(arma::mat Xl,
   return result;
 }
   
+//' Calculate variance of coefficient estimator for small effects
+//' 
+//' @param Xs matrix of genotypes for small effect markers
+//' @param Xl matrix of genotypes for large effect markers
+//' @param Hinv inverse of H matrix
+//' @param sigma2_s estimated value of sigma^2_s
+//' @param y trait values vector
+//' @param var_bl variance of beta hat l
+//' @return covariance matrix
+  
+arma::mat calc_var_betas(arma::mat Xl, 
+                         arma::mat Xs,
+                         arma::mat Hinv,
+                         double sigma2_s,
+                         arma::vec y,
+                         arma::mat var_bl){
+  //var y
+  arma::mat vy = y * y.t(); 
+  // 
+  arma::mat arg1 = sigma2_s * sigma2_s * Xs.t() * Hinv * vy * Hinv * Xs;
+  arma::mat result = arg1 + sigma2_s * sigma2_s * Xs.t() * Hinv * Xl * var_bl * Xl.t() * Hinv * Xs;
+  return result;
+}
+
