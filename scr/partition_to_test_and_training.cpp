ae004663b4856fcd4c75e070b973923938bde376
Frederick Boehm <frederick.boehm@gmail.com>
2021-09-06 19:26:46 -0400

    fixed doc & started using Col <uword>

--- scr/partition_to_test_and_training.cpp
+++ scr/partition_to_test_and_training.cpp
@@ -9,24 +9,22 @@
 //' 
 //' @param n_obs total number of subjects (test plus training)
 //' @param test_proportion proportion of subjects to place in test set
-//' @return arma::vec containing integers to indicate test set membership
+//' @return  integers to indicate test set membership
 
-arma::vec get_test_indices(int n_obs, double test_proportion){
+arma::Col<uword> get_test_indices(int n_obs, double test_proportion){
   // calculate number of subjects to put into test set
   int n_test = floor(test_proportion * n_obs);
-  arma::vec n_vec = arma::regspace(0, n_obs - 1); //create vector 0,1,2,..., n_obs - 1
-  //sample the n_test subjects
-  arma::vec test_indices = Rcpp::RcppArmadillo::sample(n_vec, n_test, false); // vector, size, replace
-  return(test_indices);
+  arma::Col<uword> result = arma::randperm(n_obs, n_test); //randomly sample without replacement from the integers 0,1,...,n_obs - 1 and return n_test of them.
+  return(result);
 }
 
-//' Subset entire set of subjects' genotype data 
+//' Subset a matrix's rows by indices 
 //' 
 //' @param mat a matrix, eg., of genotypes, for the entire cohort, with one subject per row
 //' @param test_indices vector with subject indices to go into test set
-//' @return 
+//' @return matrix of genotypes for the subsetted collection of subjects
 
-arma::mat subset_matrix(arma::mat matrix, arma::vec indices){
+arma::mat subset(arma::mat matrix, arma::Col<uword> indices){
   arma::mat result = matrix.rows(indices);
   return(result);
 }
@@ -35,9 +33,9 @@ arma::mat subset_matrix(arma::mat matrix, arma::vec indices){
 //' 
 //' @param vector a vector
 //' @param indices arma::vec of indices to indicate which entries to extract 
-//' @return an arma::vec with a subset of the entries in vector
+//' @return vector of values for the subsetted collection of subjects
 
-arma::vec subset_vector(arma::vec vector, arma::vec indices){
+arma::vec subset(arma::vec vector, arma::Col<uword> indices){
   arma::vec result = vector.elem(indices);
   return(result);
 }
