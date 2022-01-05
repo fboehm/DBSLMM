28fb2684a7291c0decb565a6d79e2073ee6a5ec8
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-01-05 16:12:45 -0500

    fixed hpp to match all args

--- scr/dbslmmfit.hpp
+++ scr/dbslmmfit.hpp
@@ -42,11 +42,11 @@ public:
 	// estimate large and small effect for each block
 	arma::vec calcBlock(int n_ref, int n_obs, double sigma_s, vector<int> idv, string bed_str, 
 				  vector <INFO> info_s_block_full, vector <INFO> info_l_block_full, int num_s_block, int num_l_block, 
-				  vector <EFF> &eff_s_block, vector <EFF> &eff_l_block);
+				  vector <EFF> &eff_s_block, vector <EFF> &eff_l_block, arma::Col <arma::uword> training_indices);
 	// estimate only small effect for each block
 	arma::vec calcBlock(int n_ref, int n_obs, double sigma_s, vector<int> idv, string bed_str, 
 				  vector <INFO> info_s_block_full, int num_s_block, 
-				  vector <EFF> &eff_s_block);
+				  vector <EFF> &eff_s_block, arma::Col <arma::uword> training_indices);
 	// solve x=Ab
 	vec PCGv(mat A, vec b, size_t maxiter, const double tol); 
 	// solve x=AB
