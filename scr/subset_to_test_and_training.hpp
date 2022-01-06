84963ff282b1e26dd1c00af77698fd5372d970f1
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-01-05 20:19:10 -0500

    added to hpp file

--- scr/subset_to_test_and_training.hpp
+++ scr/subset_to_test_and_training.hpp
@@ -17,3 +17,8 @@ arma::uvec convert_string_to_indices(std::string in_string);
 
 arma::uvec read_indices_file(const std::vector <std::string> filepath);
 
+template<typename C1, typename C2>
+void castContainer(const C1& source, C2& destination);
+
+template<typename T, typename T2>
+std::vector<T>& operator<<(std::vector<T>& v, T2 t);
